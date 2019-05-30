7248d62f4ce9555bb203bcebe80b3cd90c05695a
Maksim Bogatov <maxbog@inbox.ru>
2019-05-30 16:28:02 +0300

    Урок 8

--- WebAPIServiceEmp/Models/Data.cs
+++ WebAPIServiceEmp/Models/Data.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
-
+//Создать базу Employees Создание таблиц и их наполнение происходит при запуске клиентской части
 namespace WebAPIServiceEmp.Models
 {
     
