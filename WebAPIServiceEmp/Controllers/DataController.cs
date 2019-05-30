438d67757f433694673505635b947df9364da26e
Maksim Bogatov <maxbog@inbox.ru>
2019-05-30 16:26:49 +0300

    Урок 8

---
+++ WebAPIServiceEmp/Controllers/DataController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIServiceEmp.Models;
+
+namespace WebAPIServiceEmp.Controllers
+{
+    public class DataController : ApiController
+    {
+        private Data data = new Data();
+
+
+        [Route("getlist")]
+        public List<Employee> Get() => data.GetList();
+
+        [Route("getlist/{id}")]
+        public Employee GetPeople(int id) => data.GetPeopleById(id);
+
+        [Route("addpeople")]
+        public HttpResponseMessage Post([FromBody]Employee value)
+        {
+            if (data.AddPeople(value))
+                return Request.CreateResponse(HttpStatusCode.Created);
+            else return Request.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+    }
+}
